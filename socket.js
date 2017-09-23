612bd0ecb0b9ecead06c32597e39e92743f9c12b
argonlaser <vkvenkat94@gmail.com>
2017-09-24 01:18:24 +0530

    add game on game.join event

--- socket.js
+++ socket.js
@@ -3,13 +3,18 @@ const logger = require('./Logger/winston.js')
 
 let socket = function (socket) {
   // handle all connected related events.
-  console.log('Socket connected = ', socket.id)
+  logger.info('Socket connected = ', socket.id)
 
   let user = new Player(socket)
-  let game = global.gamePool.AllocateGameForUser(user)
+  let game
 
   socket.on('user.strike', function (payload) {
     user.stike(payload)
+  })
+
+  socket.on('game.join', function (payload) {
+    logger.debug('game.join event :', socket.id)
+    game = global.gamePool.AllocateGameForUser(user)
   })
 
   socket.on('user.bingo', function (payload) {
@@ -19,7 +24,7 @@ let socket = function (socket) {
   // handle all disconnected related events
   socket.on('disconnect', function () {
     // remove player from the game
-    game.removePlayer(user)
+    game && game.removePlayer(user)
     logger.info('Disconnected: ' + socket.id)
   })
 }
